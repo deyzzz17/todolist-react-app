d629f29d8ad19a5f93df1ce6fbe7f270fda50f90
deyzzz17 <swodey@free.fr>
2026-03-06 20:30:40 +0100

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
-import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <Analytics mode="production" />;
   </StrictMode>,
 );
